221829297debdcda833bfefa5d5bf9615dc63b86
xavmor <xavmoroni@gmail.com>
2018-12-07 20:48:18 -0700

    Working Thermal Camera needs some work though.

--- MSP432 Code/AMG88xx.c
+++ MSP432 Code/AMG88xx.c
@@ -45,7 +45,7 @@ void readThermalPixels(uint16_t *, uint8_t size);
 eUSCI_I2C_MasterConfig i2cConfig =
 {
     EUSCI_B_I2C_CLOCKSOURCE_SMCLK,          // Clock that we will source from
-    3000000,                                // Desired System Clock Rate 3MHz
+    48000000,                                // Desired System Clock Rate 3MHz
     EUSCI_B_I2C_SET_DATA_RATE_400KBPS,      // Desired I2C Clock Rate
     0,                                      // No byte counter threshold
     EUSCI_B_I2C_NO_AUTO_STOP                // No Autostop
@@ -60,17 +60,19 @@ eUSCI_I2C_MasterConfig i2cConfig =
 void AMG88xxInit(uint8_t addr)
 {
     // Step 0: Initialize pins pin3.6 and 3.7
-    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN6);          // Activate the pull up resistors
-    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P3, GPIO_PIN7);
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P3, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
-    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P3, GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
-
-
-    // Step 1: Initialize & enable I2C settings
-    I2C_initMaster(EUSCI_B2_BASE, &i2cConfig);
-    I2C_setSlaveAddress(EUSCI_B2_BASE, 0x69);
-    I2C_setMode(EUSCI_B2_BASE, EUSCI_B_I2C_TRANSMIT_MODE);      // Master in transmit mode
-    I2C_enableModule(EUSCI_B2_BASE);
+//    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P6, GPIO_PIN5);          // Activate the pull up resistors
+//    GPIO_setAsInputPinWithPullUpResistor(GPIO_PORT_P6, GPIO_PIN4);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6, GPIO_PIN5, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P6, GPIO_PIN4, GPIO_PRIMARY_MODULE_FUNCTION);
+//
+//
+//    // Step 1: Initialize & enable I2C settings
+    /* Disable I2C module to make changes */
+    I2C_disableModule(EUSCI_B1_BASE);
+    I2C_initMaster(EUSCI_B1_BASE, &i2cConfig);
+    I2C_setSlaveAddress(EUSCI_B1_BASE, 0x69);
+    I2C_setMode(EUSCI_B1_BASE, EUSCI_B_I2C_TRANSMIT_MODE);      // Master in transmit mode
+    I2C_enableModule(EUSCI_B1_BASE);
 
 
      // Step 2: Initialize Thermal Sensor registers
@@ -87,23 +89,23 @@ void AMG88xxInit(uint8_t addr)
     /* Wait until ready to write PL */
 
     /* Clear any existing interrupt flag PL */
-//        I2C_clearInterruptFlag(EUSCI_B2_BASE,
+//        I2C_clearInterruptFlag(EUSCI_B1_BASE,
 //            EUSCI_B_I2C_TRANSMIT_INTERRUPT0);
 //
-//    while (I2C_isBusBusy(EUSCI_B2_BASE));
+    while (I2C_isBusBusy(EUSCI_B1_BASE));
 
     // Enter Normal Mode
-    I2C_masterSendMultiByteStart(EUSCI_B2_BASE, Reg_PowerControl);
-    I2C_masterSendMultiByteFinish(EUSCI_B2_BASE, PowerControl_NormalMode);
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE, Reg_PowerControl);
+    I2C_masterSendMultiByteFinish(EUSCI_B1_BASE, PowerControl_NormalMode);
     // Software Reset
-    I2C_masterSendMultiByteStart(EUSCI_B2_BASE, Reg_Reset);
-    I2C_masterSendMultiByteFinish(EUSCI_B2_BASE, Reset_InitialReset);
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE, Reg_Reset);
+    I2C_masterSendMultiByteFinish(EUSCI_B1_BASE, Reset_InitialReset);
     // Disable interrupts
-    I2C_masterSendMultiByteStart(EUSCI_B2_BASE, Reg_InterruptState);
-    I2C_masterSendMultiByteFinish(EUSCI_B2_BASE, InterruptIntModeDiff | InterruptIntEnableReAct);
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE, Reg_InterruptState);
+    I2C_masterSendMultiByteFinish(EUSCI_B1_BASE, InterruptIntModeDiff | InterruptIntEnableReAct);
     // Set to 10 Frames a second
-    I2C_masterSendMultiByteStart(EUSCI_B2_BASE, Reg_FrameRate);
-    I2C_masterSendMultiByteFinish(EUSCI_B2_BASE, FrameRate10FPS);
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE, Reg_FrameRate);
+    I2C_masterSendMultiByteFinish(EUSCI_B1_BASE, FrameRate10FPS);
 }
 
 
@@ -125,13 +127,13 @@ void readThermalPixels(uint16_t * buff, uint8_t size)
 {
     //uint16_t recast;
     //float converted;
-    const uint8_t bytesToRead = (size << 1);          // Double the Max # of Bytes
-    uint8_t rawData[bytesToRead];
+    uint8_t bytesToRead = (size << 1);          // Double the Max # of Bytes
+    uint8_t rawData[128];
 
     uint8_t i = 0;
     // Read from the sensor HELP See if I can move this and capsulate
-    //I2C_setMode(EUSCI_B2_BASE, EUSCI_B_I2C_RECEIVE_MODE);
-    while (i <= bytesToRead)
+    //I2C_setMode(EUSCI_B1_BASE, EUSCI_B_I2C_RECEIVE_MODE);
+    while (i < 128)
     {   /*
          * Communication Process:
          *      Send a start sequence
@@ -142,10 +144,25 @@ void readThermalPixels(uint16_t * buff, uint8_t size)
          *      Read data byte from slave
          *      Send the stop sequence.
          */
-        I2C_setMode(EUSCI_B2_BASE, EUSCI_B_I2C_TRANSMIT_MODE);
-        I2C_masterSendSingleByte(EUSCI_B2_BASE, (0x80 + i));
-        I2C_setMode(EUSCI_B2_BASE, EUSCI_B_I2C_RECEIVE_MODE);
-        rawData[i] = I2C_masterReceiveSingleByte(EUSCI_B2_BASE);
+        I2C_setMode(EUSCI_B1_BASE, EUSCI_B_I2C_TRANSMIT_MODE);
+        I2C_masterSendSingleByte(EUSCI_B1_BASE, (0x80 + i));
+        I2C_setMode(EUSCI_B1_BASE, EUSCI_B_I2C_RECEIVE_MODE);
+        //rawData[i] = I2C_masterReceiveSingleByte(EUSCI_B1_BASE);
+
+        /* Wait until 2nd Byte has been output to shift register */
+            //while(!(EUSCI_B1->IFG & EUSCI_B_IFG_TXIFG0));
+            // Send the restart condition, read one byte, send the stop condition right away
+            EUSCI_B1->CTLW0 &= ~(EUSCI_B_CTLW0_TR);
+            EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTT;
+            while(MAP_I2C_masterIsStartSent(EUSCI_B1_BASE));
+            EUSCI_B1->CTLW0 |= EUSCI_B_CTLW0_TXSTP;
+            while(!(EUSCI_B1->IFG & EUSCI_B_IFG_RXIFG0));
+            rawData[i] = EUSCI_B1->RXBUF;
+            //rawData[i] = I2C_slaveGetData(EUSCI_B1_BASE);
+
+        //rawData[i+1] = I2C_masterReceiveMultiByteNext(EUSCI_B1_BASE);
+        //I2C_masterReceiveMultiByteStop(EUSCI_B1_BASE);
+        //while (EUSCI_B_I2C_SENDING_STOP == I2C_masterIsStopSent(EUSCI_B1_BASE));
         i++;
     }
 
@@ -153,14 +170,16 @@ void readThermalPixels(uint16_t * buff, uint8_t size)
      * one actual pixel value. We need to combine our 128 data into 64 data pieces.
      */
     i = 0;
+    uint8_t j = 0;
     uint16_t highVal;
     uint16_t lowVal;
-    while (i < bytesToRead)
+    while (i < 128)
     {
         lowVal = rawData[i];
         highVal = rawData[i+1];
-        buff[i] = ((highVal << 8) | lowVal);
+        buff[j] = ((highVal << 8) + lowVal);
         i += 2;
+        j++;
     }
 }
 
--- MSP432 Code/main.c
+++ MSP432 Code/main.c
@@ -46,12 +46,10 @@
 /* Graphic library context */
 Graphics_Context g_sContext;
 
-//Graphics_Rectangle myRectangle;
-
 /* ADC results buffer */
 static uint16_t resultsBuffer[2];
 
-// Prototype
+// Prototyoe
 void DisplayToLCD(uint16_t *);
 
 /*
@@ -100,41 +98,14 @@ int main(void)
                                     30,
                                     OPAQUE_TEXT);
 
-<<<<<<< HEAD
     Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 50, 50, ClrAliceBlue);
     Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 50, 51, ClrAliceBlue);
     Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 51, 50, ClrAliceBlue);
     Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 50, ClrAliceBlue);
     Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 51, ClrAliceBlue);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 48, 50, ClrAliceBlue);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 49, ClrAliceBlue);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 51, 49, ClrAliceBlue);
-
-    // Dallin
-    Graphics_Rectangle myRectangle;
-    myRectangle.xMin = 16;
-    myRectangle.yMin = 16;
-    myRectangle.xMax = 31;
-    myRectangle.yMax = 31;
-
-    Graphics_fillRectangle(&g_sContext, &myRectangle);
-
-=======
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 50, 50, ClrYellowGreen);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 50, 51, ClrYellowGreen);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 51, 50, ClrYellowGreen);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 50, ClrYellowGreen);
-    Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 51, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 48, 50, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 49, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 51, 49, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 52, 50, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 53, 51, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 54, 52, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 55, 53, ClrYellowGreen);
-        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 56, 54, ClrYellowGreen);
->>>>>>> 66b35d49145b1437d1bb127a87bfb3919dfdec3d
-
+        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 48, 50, ClrAliceBlue);
+        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 49, 49, ClrAliceBlue);
+        Graphics_drawPixelOnDisplay(&g_sCrystalfontz128x128, 51, 49, ClrAliceBlue);
 
     /* Configures Pin 6.0 and 4.4 as ADC input */
     //GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P6, GPIO_PIN0, GPIO_TERTIARY_MODULE_FUNCTION);
@@ -236,7 +207,7 @@ void ADC14_IRQHandler(void)
 void DisplayToLCD(uint16_t * buffer)
 {
     /* Buffer contains a max of 64 pixel data. So for every pixel we will assign it to
-     * 16 pixels. 1x1 = 8x8. Reason is because our LCD screen is huge compared to the
+     * 16 pixels. 1x1 = 8x8. Reason is becuase our LCD screen is huge compared to the
      * thermal sensor camera thing.
      */
     int i, j;
@@ -264,7 +235,7 @@ void DisplayToLCD(uint16_t * buffer)
     }
 
 
-/* Comment test*/
+
 
 }
 
